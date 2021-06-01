f8ad26c842bf4ade1c380517b26b9508f5612ed8
Araceli Valdovinos <valdovinosaraceli50@gmail.com>
2021-06-01 13:33:56 -0500

    Add sub., mul., div. functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,16 @@
 function add(number1, number2){
 	return number1 + number2;
 }
-
+function sub(number1, number2){
+	return number1 - number2;
+}
+function mul(number1, number2){
+	return number1 * number2;
+}
+function div(number1, number2){
+	return number1 / number2;
+}
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"))
-const result = add(number1, number2)
+const result = div(number1, number2)
 alert(result);
